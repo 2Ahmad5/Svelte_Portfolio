0c35e28e2022a09be7e134da33043a50d7f9d5df
Ahmad Choudhary <chahmad2005@gmail.com>
2024-05-23 01:04:59 -0400

    oof

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,9 +1,33 @@
-<h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
+<div class="h-screen grid justify-center items-center grid-cols-3 grid-rows-3">
+    <div></div>
+    <div class="flex justify-center items-center">
+        <div class="rounded-full bg-[#7a5a18] w-3/6 h-3/6"></div>
+    </div>
+    <div></div>
+    <div></div>
+    <div>
+        <h1 class="text-center max-w-fit text-[#7a5a18] font-custom text-7xl">AHMAD CHOUDHARY</h1>
+    </div>
+    
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+</div>
 
 <style lang="postcss">
-  :global(html) {
-    background-color: white;
+    @import '../app.css';
+   :global(html){
+    background: linear-gradient(135deg, #000000, #2b0000, #000000);
+
+  }
+
+  h1{
+    
+    text-shadow:
+   -1px -1px 0 #f5e045,  
+    1px -1px 0 #f5e045,
+    -1px 1px 0 #f5e045,
+     1px 1px 0 #f5e045;
   }
 </style>
