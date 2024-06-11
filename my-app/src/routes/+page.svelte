e89e31a4b015272a4a921a3096711955229fd232
Ahmad Choudhary <chahmad2005@gmail.com>
2024-06-11 14:57:53 -0400

    oof
    Co-authored-by: Siven Panda <sivepanda@users.noreply.github.com>

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <svelte:head>
-  <script src="https://kit.fontawesome.com/467e545fea.js" crossorigin="anonymous"></script>
+  
 </svelte:head>
+
 
 <div class="w-screen h-full flex flex-col justify-center items-center cursor-fancy overflow-x-hidden">
     <h1 class="text-5xl text-center mb-5">Ahmad Choudhary</h1>
@@ -9,7 +10,7 @@
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum djhfbv hjsbdv.</p>
     </div>
     <hr class="w-[30svh] h-0.5 mx-auto bg-white border-0 rounded my-4">
-    <a href="/src/routes/projects/+page.svelte" class="cursor-fancy w-[20svh] h-[6svh] flex justify-center items-center border-2 text-[#a5a4a3] border-white rounded-3xl mt-5 gap-2 hover:bg-white hover:text-black group">
+    <a href="/projects" class="cursor-fancy w-[20svh] h-[6svh] flex justify-center items-center border-2 text-[#a5a4a3] border-white rounded-3xl mt-5 gap-2 hover:bg-white hover:text-black group">
         <div>
             <h2 class="transition-opacity duration-300 ease-in-out">Lets Go</h2>
         </div>
--- my-app/src/routes/projects/+page.svelte
+++ my-app/src/routes/projects/+page.svelte
@@ -1,39 +1,82 @@
 <script>
- 
+    
+    let resume_hidden = false;
+    let proj = true;
+    let res = false;
+    let edu = false;
+    let abt = false;
+
+    function projectClick(){
+        proj = true;
+        res = false;
+        edu = false;
+        abt = false;
+    }
+
+    function resumeClick(){
+        proj = false;
+        res = true;
+        edu = false;
+        abt = false;
+    }
+
+    function educationClick(){
+        proj = false;
+        res = false;
+        edu = true;
+        abt = false;
+    }
+
+    function aboutClick(){
+        proj = false;
+        res = false;
+        edu = false;
+        abt = true;
+    }
+
 </script>
 
-
-<head>
+<svelte:head>
     <script src="https://kit.fontawesome.com/467e545fea.js" crossorigin="anonymous"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<div class="flex cursor-fancy justify-center items-center w-full flex-col gap-[30px]">
-    <div class="grid justify-items-center justify-self-center w-full text-[1vh] md:text-sm text-white items-end pb-[5px] max-w-[50vw] min-w-[400px] grid-cols-4 border-b-white border-2 border-transparent mt-[10vh]">
-        <h2>Projects</h2>
-        <h2>Resume</h2>
-        <h2>Education</h2>
-        <h2>About Me</h2>
-    </div>
-    <div class="smallonly">
+</svelte:head>
 
+<div class="flex cursor-fancy items-center w-full min-h-[100vh] flex-col gap-[30px]">
+    <div class="grid w-full text-[1vh] md:text-sm text-white pb-[5px] max-w-[50vw] min-w-[400px] grid-cols-4 border-b-white border-2 border-transparent mt-[10vh] absolute top-0">
+        <button class="hover:cursor-fancy {proj ? 'yes-click' : 'no-click'}" on:click={projectClick}>Projects</button>
+        <button class="hover:cursor-fancy {res ? 'yes-click' : 'no-click'}" on:click={resumeClick}>Resume</button>
+        <button class="hover:cursor-fancy {edu ? 'yes-click' : 'no-click'}" on:click={educationClick}>Education</button>
+        <button class="hover:cursor-fancy {abt ? 'yes-click' : 'no-click'}" on:click={aboutClick}>About Me</button>
     </div>
-    <h1 class="text-5xl mt-[5vh] mb-[2vh] text-center">Software Design and Web Dev</h1>
+    
+    <div class="{proj ? 'initial' : 'hidden'} flex gap-[2vh] flex-col items-center">
+    <h1 class="text-5xl mt-[25vh] mb-[2vh] text-center">Software Design and Web Dev</h1>
+    <div class="smallonly hidden">
+        <div class="w-[85vw] h-[20vh] retro rounded-xl"></div>
+        <div class="max-w-[85vw] bg-[#404040] p-[15px] rounded-xl text-justify text-sm mt-[2vh]">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="w-[85vw] h-[20vh] nuwrld rounded-xl mt-[2vh]"></div>
+        <div class="max-w-[85vw] bg-[#404040] p-[15px] rounded-xl text-justify text-sm mt-[2vh]">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+    </div>
     <div class="flex gap-[2vw] bigonly">
-        <a href="https://sivepanda.github.io/tsawebmaster23/" class="hover:shadow-[-4px_6px_10px_0_rgba(0,0,0,0.6)] retro w-[35vw] h-[30vh] rounded-xl max-w-[600px] cursor-fancy hover:-translate-y-3 ease-in-out duration-200"></a>
+        <a href="https://sivepanda.github.io/tsawebmaster23/" class="rot hover:shadow-[-4px_6px_10px_0_rgba(0,0,0,0.6)] retro w-[35vw] h-[30vh] rounded-xl max-w-[600px] cursor-fancy hover:-translate-y-3 ease-in-out duration-200"></a>
         <div class="max-w-[35vw] bg-[#404040] p-[15px] rounded-xl text-justify text-sm">
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
     </div>
     <div class="flex gap-[2vw] bigonly">
-        <div class="max-w-[35vw] bg-[#404040] p-[15px] rounded-xl text-justify text-sm">
+        <div class="max-w-[35vw] bg-[#404040] p-[15px] rounded-xl text-justify text-sm ease-in-out duration-200">
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
-        <a href="https://sivepanda.github.io/tsawebmaster22/public_html/" class="hover:shadow-[4px_6px_10px_0_rgba(0,0,0,0.6)] nuwrld w-[35vw] h-[30vh] rounded-xl max-w-[600px] cursor-fancy hover:-translate-y-3 ease-in-out duration-200"></a>
+        <a href="https://sivepanda.github.io/tsawebmaster22/public_html/" class="roty hover:shadow-[4px_6px_10px_0_rgba(0,0,0,0.6)] nuwrld w-[35vw] h-[30vh] rounded-xl max-w-[600px] cursor-fancy hover:-translate-y-3 ease-in-out duration-200"></a>
         
     </div>
     <h1 class="text-5xl mt-[5vh] mb-[2vh] text-center">Machine Learning</h1>
     <div class="flex  xl:flex-row flex-col items-center justify-center">
-        <div class="w-[40vh] h-[32vh] card-back border-2 rounded-xl xl:w-[23vw] flex flex-col p-[20px] gap-[2vh]">
+        <div class="w-[90vw] h-[32vh] card-back border-2 rounded-xl xl:w-[23vw] flex flex-col p-[20px] gap-[2vh]">
 
             <i class="fa-solid fa-basketball text-4xl pt-[10%]" style="color: #ffffff;"></i>
             
@@ -41,7 +84,7 @@
             <p class="text-xs">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         </div>
         <div class="xl:w-[2px] xl:h-[25vh] w-[40vw] h-[2px] rounded-lg bg-white my-[1vh] mx-[1vw]"></div>
-        <div class="w-[40vh] h-[32vh] card-back border-2 rounded-xl xl:w-[23vw] flex flex-col p-[20px] gap-[2vh]">
+        <div class="w-[90vw] h-[32vh] card-back border-2 rounded-xl xl:w-[23vw] flex flex-col p-[20px] gap-[2vh]">
 
             <i class="fa-solid fa-network-wired text-4xl pt-[10%]" style="color: #ffffff;"></i>
             
@@ -50,7 +93,7 @@
         </div>
         <div class="xl:w-[2px] xl:h-[25vh] w-[40vw] h-[2px] rounded-lg bg-white my-[1vh] mx-[1vw]"></div>
 
-        <div class="w-[40vh] h-[32vh] card-back border-2 rounded-xl xl:w-[23vw] flex flex-col p-[20px] gap-[2vh]">
+        <div class="w-[90vw] h-[32vh] card-back border-2 rounded-xl xl:w-[23vw] flex flex-col p-[20px] gap-[2vh]">
 
             <i class="fa-solid fa-diagram-project text-4xl pt-[10%]" style="color: #ffffff;"></i>
             
@@ -58,13 +101,33 @@
             <p class="text-xs">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         </div>
     </div>
+    </div>
+
+    <div class="{res ? 'initial' : 'hidden'} grid grid-cols-2 h-full w-full justify-items-center">
+        <div class="flex flex-col">
+            <h1 class="text-5xl mt-[25vh] text-center">Resume</h1>
+            <a href="../../media/Resume (Ahmad Choudhary) (1).pdf"download on:mouseenter={() => {resume_hidden = true;}} on:mouseleave={() => {resume_hidden = false;}} class="resume-contain rounded-xl mt-[2vh] relative">
+                
+                <div class="{!resume_hidden ? 'opacity-100' : 'opacity-0'} resume-bkg w-[25vw] h-[70vh] rounded-xl"></div>
+                
+            </a>
+            
+            
+        </div>
+        <div>
+            <h1 class="text-5xl mt-[25vh] text-center">Skillset</h1>
+        </div>
+    </div>
+
+    <div  class="{edu ? 'initial' : 'hidden'}">vbc</div>
+    <div class="{abt ? 'initial' : 'hidden'}">asd</div>
     
-    <div class="w-full h-[5vh] mt-[2vh] bg-[#333333] flex justify-center items-center">
-        <div class="text-[1vh] md:text-sm w-full max-w-[40vw] min-w-[350px] grid grid-cols-4 items-center">
-            <p class="text-white justify-self-center">Linkedin</p>
-            <p class="text-white justify-self-center">Instagram</p>
-            <p class="text-white justify-self-center">Email</p>
-            <p class="text-white justify-self-center">Discord</p>
+    <div class="w-full h-[5vh] bg-[#333333] flex justify-center items-center relative">
+        <div class="text-[1vh] md:text-sm w-full max-w-[40vw] min-w-[350px] grid grid-cols-4 items-center absolute bottom-0">
+            <p class="text-white mb-[1.5vh] justify-self-center">Linkedin</p>
+            <p class="text-white  mb-[1.5vh] justify-self-center">Instagram</p>
+            <p class="text-white  mb-[1.5vh] justify-self-center">Email</p>
+            <p class="text-white  mb-[1.5vh] justify-self-center">Discord</p>
         </div>
     </div>
 </div>
@@ -83,7 +146,36 @@
             display: initial;
         }
     }
+
+    .resume-bkg{
+        background: url('../../media/AhmadResume.png');
+        background-position: center;
+        background-size:cover;
+        /* transition: all .1s ease-out; */
+    }
+
+    .resume-contain{
+        background: white;
+        transition: all .3s ease-out;
+    }
+
+    .resume-contain:hover {
+        transform: perspective(1000px) rotate3d(1, 0, 0, 45deg);
+    }
+
+    /* .resume-bkg:hover{
+        opacity: 0;
+        transform: perspective(1000px) rotate3d(1, 0, 0, 45deg);
+    } */
+
+    .yes-click{
+        color: white;
+    }
    
+    .no-click{
+        color: #a5a4a3;
+    }
+
     .card-back{
         background-color: rgba(0,0,0, .3) !important;
         border-color: rgba(255, 255, 255, 0.1);
@@ -96,13 +188,29 @@
         margin-left: 0;
     }
 
-    body {
+    .rot{
+        transform: perspective(1000px) rotate3d(0, 1, 0, 10deg);
+    }
+    .rot:hover{
+        transition: all .4s ease-in-out;
+        transform: perspective(1000px) rotate3d(-.4, 1, 0, 10deg) translateY(-10px) !important;
+    }
+    .roty{
+        transition: all .4s ease-in-out;
+        transform: perspective(1000px) rotate3d(0, -14, 0, 10deg);
+    }
+    .roty:hover{
+        transform: perspective(1000px) rotate3d(-.4, -14, 0, 10deg) translateY(-10px) !important;
+    }
+
+    :global(body) {
+        cursor: url('../../media/white_dot.png'), pointer;
+        min-height: 100vh;
         display: flex;
         justify-content: center;
         align-items: center;
         background: radial-gradient(circle at 24.1% 68.8%, rgb(50, 50, 50) 0%, rgb(0, 0, 0) 99.4%);
         background-size: cover;
-        min-height: 100vh;
     }
 
     .retro{
@@ -117,17 +225,16 @@
         background-size: cover;
     }
 
-    h2{
+    button{
         font-size: 140%;
         color: #a5a4a3;
         transition: color.2s ease-in-out;
     }
 
-    h2:hover{
+    button:hover{
         color:white;
     }
 
-  /* border-b-gray-600 */
   h1{    
 
     background: linear-gradient(90deg, #333 0%, #fff 40%, #333 100%);
@@ -140,4 +247,5 @@
     color: #a5a4a3;
   }
 </style>
+
 
